46be90ccf2c4526b08c8d98611dc5c8f9b5a1db3
Pedro Fiuza <pedro_fiuza@hotmail.com>
2024-07-04 10:47:39 +0100

    Created model and migration for photos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,15 +5,27 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900 dark:text-gray-100 dashboardContent">
-                        {{ __("Welcome to the Photo App ") }}
-                        <span style="color:steelblue;">{{ Auth::user()->name }}</span>
-                    </div>
+    <div class="py-2" >
+        <div > <!-- class="max-w-7xl mx-auto sm:px-6 lg:px-8" -->
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm ">
+                <div class="p-6 text-gray-900 dark:text-gray-100 dashboardWelcome">
+                    {{ __("Welcome to the Photo App ") }}
+                    <span style="color:steelblue;">{{ Auth::user()->name }}</span>                        
                 </div>
             </div>
         </div>
     </div>
+    <div class="p-4 dashboardContent">
+        <h2>Here you can download the specific files you will use to create your Album</h2> <hr class="mb-2">
+        <form action="#" method="get" enctype="multipart/form-data">
+            @csrf
+            <label for="photos">Upload photos (Max: 20 photos only) </label>
+            <input type="file" name="photos[]" id="photoUpload" multiple class="form-control">  
+            <div class="mt-2 ">
+                <button class="bg-orange-200">Upload</button>
+            </div>
+        </form>
+        
+    </div>
+    
 </x-app-layout>
